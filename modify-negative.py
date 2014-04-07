569f627ea98159659fb52f43742a2e7d25b16d3a
Paul Romano <paul.k.romano@gmail.com>
2014-04-07 19:28:57 -0400

    Updated version of modify-negative based on Fudge 4.1.0.

--- modify-negative.py
+++ modify-negative.py
@@ -8,22 +8,32 @@ import numpy as np
 from tabulate import tabulate
 from fudge.legacy.converting.endfFileToGND import endfFileToGND
 
+# File to convert
+endffile = 'n-094_Pu_239.endf'
+
 # This is the "change" parameter by which all data is adjusted
 target = 0.05
+initial_guess = 0.1
 
 # Create ReactionSuite from ENDF file
-print('Converting ENDF to GND...')
-eval, cov = endfFileToGND('n-094_Pu_239.endf', toStdOut=False, toStdErr=False)
+print('Targeting a {0} percent change in 2200 m/s cross sections\n'.format(target*100).upper())
+print('Reading data from {0}...'.format(endffile))
+translation = endfFileToGND(endffile, toStdOut=False, toStdErr=False)
+eval = translation['reactionSuite']
 
 # Reconstruct resonances
 print('Reconstructing resonances...')
 eval.reconstructResonances()
 
 # Get reactions
-originalCapture2200 = eval.getReaction('capture').getCrossSection()['linear'].getValue(0.0253)
-originalFission2200 = eval.getReaction('fission').getCrossSection()['linear'].getValue(0.0253)
-print('Original 2200 m/s capture xs = {0}'.format(originalCapture2200))
-print('Original 2200 m/s fission xs = {0}'.format(originalFission2200))
+capture = eval.getReaction('capture').getCrossSection()['linear']
+fission = eval.getReaction('fission').getCrossSection()['linear']
+originalCapture2200 = capture.getValue(0.0253)
+originalFission2200 = fission.getValue(0.0253)
+originalCaptureXS = zip(*capture[:])
+originalFissionXS = zip(*fission[:])
+print('Original 2200 m/s capture xs = {0:.3f} b'.format(originalCapture2200))
+print('Original 2200 m/s fission xs = {0:.3f} b'.format(originalFission2200))
 
 # Get RM parameters -- this returns a Fudge math.table object
 params = eval.resonances.resolved.regions[0].nativeData.resonanceParameters
@@ -37,13 +47,13 @@ colG = columnNames.index('captureWidth')
 colFA = columnNames.index('fissionWidthA')
 colFB = columnNames.index('fissionWidthB')
 
-xg = 0.1
-xf = 0.1
+xg = initial_guess
+xf = initial_guess
 iteration = 1
 while True:
     print('Iteration ' + str(iteration))
-    print('  Changing capture widths by {0} percent'.format(xg*100))
-    print('  Changing fission widths by {0} percent'.format(xf*100))
+    print('  Changing capture widths by {0:.1f}%'.format(xg*100))
+    print('  Changing fission widths by {0:.1f}%'.format(xf*100))
 
     # Save original values
     GG = [params.data[i][colG] for i in range(4)]
@@ -53,10 +63,7 @@ while True:
     # Increase capture widths and decrease fission widths for negative energy
     # resonances -- note that if the width is negative, we do the opposite
     for row in range(4):
-        if params.data[row][colG] > 0:
-            params.data[row][colG] *= 1 + xg
-        else:
-            params.data[row][colG] *= 1 - xg
+        params.data[row][colG] *= 1 + xg
         if params.data[row][colFA] > 0:
             params.data[row][colFA] *= 1 - xf
         else:
@@ -71,18 +78,22 @@ while True:
     eval.reconstructResonances()
 
     # Get 2200 m/s values
-    capture2200 = eval.getReaction('capture').getCrossSection()['linear'].getValue(0.0253)
-    fission2200 = eval.getReaction('fission').getCrossSection()['linear'].getValue(0.0253)
+    capture = eval.getReaction('capture').getCrossSection()['linear']
+    fission = eval.getReaction('fission').getCrossSection()['linear']
+    capture2200 = capture.getValue(0.0253)
+    fission2200 = fission.getValue(0.0253)
+    captureXS = zip(*capture[:])
+    fissionXS = zip(*fission[:])
 
     changeCapture = (capture2200 - originalCapture2200) / originalCapture2200
     changeFission = (fission2200 - originalFission2200) / originalFission2200
 
-    print('  2200 m/s capture xs = {0} ({1})'.format(capture2200, changeCapture))
-    print('  2200 m/s fission xs = {0} ({1})'.format(fission2200, changeFission))
+    print('  2200 m/s capture xs = {0:.3f} b ({1:.2f}%)'.format(capture2200, changeCapture*100))
+    print('  2200 m/s fission xs = {0:.3f} b ({1:.2f}%)'.format(fission2200, changeFission*100))
 
     iteration += 1
-    if (abs(target/changeCapture - 1) < 0.01 and
-        abs(target/changeFission - 1) < 0.01):
+    if (abs(target/changeCapture - 1) < target*0.1 and
+        abs(-target/changeFission - 1) < target*0.1):
         break
 
     # Reset partial widths
@@ -92,7 +103,7 @@ while True:
         params.data[row][colFB] = GFB[row]
 
     xg *= target/changeCapture
-    xf *= target/changeFission
+    xf *= -target/changeFission
 
 # Print tables
 headers = [col.name + (' ({0})'.format(col.units) if col.units else '')
@@ -101,6 +112,14 @@ print('\nORIGINAL RESONANCE PARAMETERS')
 print(tabulate(originalData[:4], headers=headers, tablefmt='grid'))
 print('\nMODIFIED RESONANCE PARAMETERS')
 print(tabulate(params.data[:4], headers=headers, tablefmt='grid'))
+
+# Display plot of original/modified
+plt.loglog(originalCaptureXS[0], originalCaptureXS[1])
+plt.loglog(captureXS[0], captureXS[1])
+plt.grid(True)
+plt.xlabel('Energy (eV)')
+plt.ylabel('Cross section (b)')
+plt.show()
 
 # Write out modified ENDF file
 #myENDF = eval.toENDF6({'verbosity': 0}, covarianceSuite=cov)
