717694f9857d38e55659ceace12e6c668c94d5b6
Paul Romano <paul.k.romano@gmail.com>
2014-04-07 19:57:07 -0400

    Added prompt neutron fission spectrum modification.

--- modify.py
+++ modify.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+import copy
+
+import matplotlib.pyplot as plt
+import numpy as np
 from fudge.legacy.converting.endfFileToGND import endfFileToGND
+
+# File to convert
+endffile = 'n-094_Pu_239.endf'
 
 # This is the "change" parameter by which all data is adjusted
 x = 0.05
 
+# ==============================================================================
 # Create ReactionSuite from ENDF file
-eval, cov = endfFileToGND('n-094_Pu_239.endf')
+print('Reading data from {0}...'.format(endffile))
+translation = endfFileToGND(endffile, toStdOut=False, toStdErr=False)
+eval = translation['reactionSuite']
+cov = translation['covarianceSuite']
 
 # Get RM parameters
 params = eval.resonances.resolved.regions[0].nativeData.resonanceParameters
@@ -17,6 +28,9 @@ columnNames = [col.name for col in params.columns]
 colN = columnNames.index('neutronWidth')
 colFA = columnNames.index('fissionWidthA')
 colFB = columnNames.index('fissionWidthB')
+
+# ==============================================================================
+print('Modifying resonance parameters...')
 
 # Increase radiation width for 0.3 eV resonance
 resonance = 0.2956243
@@ -35,6 +49,43 @@ params.data[row][colN] *= 1 + x
 
 # Decrease fission widths for 7.8 eV resonance
 params.data[row][colFA] *= 1 - x
+
+# ==============================================================================
+print('Modifying prompt neutron fission spectra...')
+
+# Get fission reaction
+fission = eval.getReaction('fission')
+
+# Get prompt fission neutron spectrum from thermal fission
+pnfs = fission.outputChannel.particles[0].distributions. \
+    components['uncorrelated'].energyComponent['pointwise']
+xys = pnfs[0]
+#x0, y0 = map(np.array, xys.copyDatatoXsAndYs())
+
+# Construct Watt fission spectrum
+a = 0.966e6
+b = 2.842e-6
+Watt = lambda E, a, b: float(np.exp(-E/a)*np.sinh(np.sqrt(b*E)))
+
+# Modify each probability by ratio of Watt spectra
+for i, (energy, prob) in enumerate(xys):
+    if np.isclose(energy, 0.0):
+        continue
+    xys[i] = [energy, prob*Watt(energy, (1+x)*a, b)/Watt(energy, a, b)]
+
+# Renormalize spectra
+integral = xys.integrate()
+for i, (energy, prob) in enumerate(xys):
+    xys[i] = [energy, prob/integral]
+
+
+# ==============================================================================
+
+# Plot data
+#xm, ym = map(np.array, zip(*xys))
+#plt.plot(x0, y0, label='Original')
+#plt.plot(xm, ym, label='Modified')
+#plt.show()
 
 # Write out modified ENDF file
 myENDF = eval.toENDF6({'verbosity': 0}, covarianceSuite=cov)
