ea0ea8388939cb71097aaf3edde1b66bc2bf6142
Paul Romano <paul.k.romano@gmail.com>
2020-11-11 07:53:21 -0600

    Make sure make-xls.py works if results file is missing values

--- make-xls.py
+++ make-xls.py
@@ -11,8 +11,11 @@ book = xlwt.Workbook(encoding='utf-8')
 sheet = book.add_sheet("Results")
 
 with open(sys.argv[1], 'r') as f:
-    for row, line in enumerate(f):
+    row = 0
+    for line in f:
         words = line.split()
+        if len(words) < 5:
+            continue
         benchmark = words[0]
         k_mean = float(words[1])
         k_unc = float(words[2])
@@ -30,14 +33,14 @@ with open(sys.argv[1], 'r') as f:
         if len(words) > 5:
             sheet.write(row, 5, atlf_mean)
             sheet.write(row, 6, atlf_unc)
+        row += 1
 
 
-sheet.write(row+1, 0, "AVERAGE")
-cells = "B{0}:B{1}".format(1, row+1)
-cellsStdev = "C{0}:C{1}".format(1, row+1)
-sheet.write(row+1, 1, xlwt.Formula("AVERAGE({0})".format(cells)))
-sheet.write(row+1, 2, xlwt.Formula("SQRT(SUMSQ({0}))/COUNT({0})".format(
-            cellsStdev)))
+sheet.write(row, 0, "AVERAGE")
+cells = "B{0}:B{1}".format(1, row)
+cellsStdev = "C{0}:C{1}".format(1, row)
+sheet.write(row, 1, xlwt.Formula("AVERAGE({0})".format(cells)))
+sheet.write(row, 2, xlwt.Formula("SQRT(SUMSQ({0}))/COUNT({0})".format(cellsStdev)))
 
 sheet.col(0).width = 256*40
 book.save(sys.argv[1] + '.xls')
